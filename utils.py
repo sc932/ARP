0bd5a6f8219347017bc2ea23de57ee613beb8eae
Scott Clark <zephyr3.14@gmail.com>
2023-10-10 11:07:40 -0700

    fix error when radius of vector is near 0

--- utils.py
+++ utils.py
@@ -4,7 +4,8 @@ def polar_from_x_y(x_orig, y_orig, x_offset=0, y_offset=0):
     x = x_orig - x_offset # optionally subtract off mean, median, etc
     y = y_orig - y_offset
     r = numpy.sqrt(x**2 + y**2)
-    if r > 0:
+    theta = 0
+    if r > 1e-8:
         if x > 0 and y > 0:
             theta = numpy.arctan(y / x)
         if x < 0 and y > 0:
@@ -14,5 +15,6 @@ def polar_from_x_y(x_orig, y_orig, x_offset=0, y_offset=0):
         if x < 0 and y < 0:
             theta = numpy.pi + numpy.arctan(-y / -x)
     else:
+        r = 0
         theta = 0
     return r, theta
